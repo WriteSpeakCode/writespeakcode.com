4391a564d9cb4820d6f11ec6c00d432ceee3d41e
Nicola Beuscher <nbeuscher@gmail.com>
2019-03-10 21:21:52 -0400

    Add Jest for unit testing

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from "@storybook/react"
 
 function loadStories() {
-  const req = require.context("../src", true, /\.stories\.js$/)
+  const req = require.context("../__stories__", true, /\.stories\.js$/)
   req.keys().forEach(filename => req(filename))
 }
 
