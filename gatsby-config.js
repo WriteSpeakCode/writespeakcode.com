bf22ceff76e872804b8cc94af2ea677446bbc0bb
Nicola Beuscher <nbeuscher@gmail.com>
2019-03-17 11:07:39 -0400

    Update site metadata and plugin manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Write/Speak/Code`,
+    description: `Write/Speak/Code is on a mission to promote the visibility and leadership of technologists with marginalized genders through peer-led professional development.`,
+    author: `@writespeakcode`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,17 +18,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `writespeakcode.com`,
+        short_name: `W/S/C`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#172751`,
+        theme_color: `#EB6851`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        include_favicon: true,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
+    "gatsby-plugin-offline",
   ],
 }
