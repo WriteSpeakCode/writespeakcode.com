a1299c8aa0fecc227479b1c745ba58337171e34a
Nicola Beuscher <nbeuscher@gmail.com>
2019-03-17 11:21:25 -0400

    Reorganize components into folders and add css modules

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import Header from "../header"
 
 const Layout = ({ children }) => (
   <StaticQuery
