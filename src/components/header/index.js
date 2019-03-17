d47beebf890ea6e3d40072f503b4298a26c013b3
Nicola Beuscher <nbeuscher@gmail.com>
2019-03-17 15:57:49 -0400

    Add logo to header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,11 +2,16 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import styles from "./header.module.css"
+import logo from "../../images/logos/wsc-logo-fullcolor.png"
+
 const Header = ({ siteTitle }) => (
   <header>
     <div>
       <h1>
-        <Link to="/">{siteTitle}</Link>
+        <Link to="/">
+          <img src={logo} alt="Write Speak Code logo" className={styles.logo} />
+        </Link>
       </h1>
     </div>
   </header>
