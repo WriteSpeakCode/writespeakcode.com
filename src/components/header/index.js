3669a2a21fd5f665dd51262b351400b306203bbd
Nicola Beuscher <nbeuscher@gmail.com>
2019-03-19 22:42:31 -0400

    Add flow

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,11 @@
+// @flow
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
 import styles from "./header.module.css"
 
-const Header = ({ siteTitle }) => (
+const Header = ({ siteTitle }: { siteTitle: string }) => (
   <header>
     <div>{siteTitle}</div>
   </header>
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,4 @@
+// @flow
 import React from "react"
 
 import Layout from "../layouts/default"
