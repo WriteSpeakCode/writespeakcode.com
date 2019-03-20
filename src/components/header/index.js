4969135eb70915ef186ae2ae7b8ee7325954c05c
Nicola Beuscher <nbeuscher@gmail.com>
2019-03-19 21:42:57 -0400

    Update header snapshots

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,17 +3,10 @@ import PropTypes from "prop-types"
 import React from "react"
 
 import styles from "./header.module.css"
-import logo from "../../images/logos/wsc-logo-fullcolor.png"
 
 const Header = ({ siteTitle }) => (
   <header>
-    <div>
-      <h1>
-        <Link to="/">
-          <img src={logo} alt="Write Speak Code logo" className={styles.logo} />
-        </Link>
-      </h1>
-    </div>
+    <div>{siteTitle}</div>
   </header>
 )
 
