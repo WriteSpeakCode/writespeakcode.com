aeea52eb10675954b67c3a39b592b2a1646ed835
Nicola Beuscher <nbeuscher@gmail.com>
2019-03-25 22:02:10 -0400

    Add logo and social links to header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,48 @@
-// import { Link } from "gatsby"
+import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-// import styles from "./header.module.css"
+import styles from "./header.module.css"
+import logo from "../../images/logos/wsc-logo-fullcolor.png"
 
 const Header = ({ siteTitle }) => (
   <header>
-    <div>{siteTitle}</div>
+    <Link to="/">
+      <img
+        src={logo}
+        alt="Write/Speak/Code logo in full color"
+        className={styles.logo}
+      />
+    </Link>
+    <ul>
+      <li>
+        <a
+          href="https://twitter.com/writespeakcode"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://instagram.com/writespeakcode"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Instagram
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://linkedin.com/writespeakcode"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </li>
+    </ul>
   </header>
 )
 
