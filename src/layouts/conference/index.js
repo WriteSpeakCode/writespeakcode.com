ab0761afa38a682053d03ab67fe710b486a17405
Nicola Beuscher <nbeuscher@gmail.com>
2019-03-25 22:10:06 -0400

    Remove some base css

--- src/layouts/conference/index.js
+++ src/layouts/conference/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import PropTypes from "prop-types"
 
 import "../base.css"
-import styles from "./conference.module.css"
 
 const Layout = ({ children }) => <main>{children}</main>
 
