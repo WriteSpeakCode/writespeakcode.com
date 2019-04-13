986416710779e03b30ed8d05c0ce4ef2e86e3014
Nicola Beuscher <nbeuscher@gmail.com>
2019-04-13 17:12:44 -0400

    Load logo in header via static query

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
+import { Link, StaticQuery } from 'gatsby'
+import Img from 'gatsby-image'
 import VisuallyHidden from '@reach/visually-hidden'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -13,75 +13,86 @@ import {
 } from '@fortawesome/free-brands-svg-icons'
 
 import styles from './header.module.css'
-import logo from '../../images/logos/wsc-logo-fullcolor.png'
 
-const Header = ({ siteTitle }) => (
-  <header className={styles.container}>
-    <Link to="/">
-      <VisuallyHidden>Home</VisuallyHidden>
-      <img
-        src={logo}
-        alt="Write/Speak/Code logo in full color"
-        className={styles.logo}
-        aria-hidden
-      />
-    </Link>
-    <div className={styles.social}>
-      <a
-        className={styles.link}
-        href="https://twitter.com/writespeakcode"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <VisuallyHidden>Twitter</VisuallyHidden>
-        <FontAwesomeIcon icon={faTwitter} aria-hidden />
-      </a>
-      <a
-        className={styles.link}
-        href="https://www.meetup.com/pro/writespeakcode/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <VisuallyHidden>Meetup</VisuallyHidden>
-        <FontAwesomeIcon icon={faMeetup} aria-hidden />
-      </a>
-      <a
-        className={styles.link}
-        href="https://linkedin.com/company/writespeakcode"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <VisuallyHidden>LinkedIn</VisuallyHidden>
-        <FontAwesomeIcon icon={faLinkedinIn} aria-hidden />
-      </a>
-      <a
-        className={styles.link}
-        href="https://instagram.com/writespeakcode"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <VisuallyHidden>Instagram</VisuallyHidden>
-        <FontAwesomeIcon icon={faInstagram} aria-hidden />
-      </a>
-      <a
-        className={styles.link}
-        href="https://github.com/writespeakcode"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <VisuallyHidden>Github</VisuallyHidden>
-        <FontAwesomeIcon icon={faGithub} aria-hidden />
-      </a>
-      <a
-        className={styles.link}
-        href="https://www.patreon.com/writespeakcode"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <VisuallyHidden>Patreon</VisuallyHidden>
-        <FontAwesomeIcon icon={faPatreon} aria-hidden />
-      </a>
-      {/*
+const Header = () => (
+  <StaticQuery
+    query={graphql`
+      query LogoQuery {
+        file(relativePath: { eq: "logos/wsc-logo-fullcolor.png" }) {
+          childImageSharp {
+            fixed(width: 250) {
+              ...GatsbyImageSharpFixed_tracedSVG
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <header className={styles.container}>
+        <Link to="/">
+          <VisuallyHidden>Home</VisuallyHidden>
+          <Img
+            fixed={data.file.childImageSharp.fixed}
+            alt="Write/Speak/Code logo in full color"
+            className={styles.logo}
+          />
+        </Link>
+        <div className={styles.social}>
+          <a
+            className={styles.link}
+            href="https://twitter.com/writespeakcode"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <VisuallyHidden>Twitter</VisuallyHidden>
+            <FontAwesomeIcon icon={faTwitter} aria-hidden />
+          </a>
+          <a
+            className={styles.link}
+            href="https://www.meetup.com/pro/writespeakcode/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <VisuallyHidden>Meetup</VisuallyHidden>
+            <FontAwesomeIcon icon={faMeetup} aria-hidden />
+          </a>
+          <a
+            className={styles.link}
+            href="https://linkedin.com/company/writespeakcode"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <VisuallyHidden>LinkedIn</VisuallyHidden>
+            <FontAwesomeIcon icon={faLinkedinIn} aria-hidden />
+          </a>
+          <a
+            className={styles.link}
+            href="https://instagram.com/writespeakcode"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <VisuallyHidden>Instagram</VisuallyHidden>
+            <FontAwesomeIcon icon={faInstagram} aria-hidden />
+          </a>
+          <a
+            className={styles.link}
+            href="https://github.com/writespeakcode"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <VisuallyHidden>Github</VisuallyHidden>
+            <FontAwesomeIcon icon={faGithub} aria-hidden />
+          </a>
+          <a
+            className={styles.link}
+            href="https://www.patreon.com/writespeakcode"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <VisuallyHidden>Patreon</VisuallyHidden>
+            <FontAwesomeIcon icon={faPatreon} aria-hidden />
+          </a>
+          {/*
       <div className={styles.cta}>
         <a
           href="/"
@@ -92,16 +103,9 @@ const Header = ({ siteTitle }) => (
         </a>
       </div>
       */}
-    </div>
-  </header>
+        </div>
+      </header>
+    )}
+  />
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
 export default Header
