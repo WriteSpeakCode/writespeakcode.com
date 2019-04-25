453c747df9e6145ecb15c93af679fb807936a844
Nicola Beuscher <nbeuscher@gmail.com>
2019-04-24 22:59:01 -0400

    Begin conference page layout

--- src/layouts/conference/index.js
+++ src/layouts/conference/index.js
@@ -2,8 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import '../base.css'
+import styles from './conference.module.css'
 
-const Layout = ({ children }) => <main>{children}</main>
+const Layout = ({ children }) => (
+  <div className={styles.container}>
+    <section>
+      <h1>2019 Conference</h1>
+    </section>
+    <nav>
+      <ul>
+        <li>Logo</li>
+        <li>Speakers</li>
+        <li>Workshops</li>
+        <li>Location</li>
+        <li>Schedule</li>
+        <li>Sponsors</li>
+        <li>Tickets</li>
+      </ul>
+    </nav>
+    <main>{children}</main>
+  </div>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/2019/index.js
+++ src/pages/2019/index.js
@@ -9,16 +9,6 @@ const IndexPage = () => (
       title="2019 Conference"
       keywords={[`tech`, `diversity`, `conference`]}
     />
-    <h1>2019 Conference</h1>
-    <h2>
-      We're on a mission to promote visibility and leadership of technologists
-      with marginalized genders through peer-led professional development.
-    </h2>
-    <h3>
-      Write/Speak/Code is a 501c3, tax-exempt non-profit that hosts an annual
-      conference and meetup events designed to help you fully own your expertise
-      through writing, speaking, and open source.
-    </h3>
   </Layout>
 )
 
