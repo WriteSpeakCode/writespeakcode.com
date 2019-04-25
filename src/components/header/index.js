4cd3d64523674f7e5ead764c9b26f8697ac81185
Nicola Beuscher <nbeuscher@gmail.com>
2019-04-24 22:54:35 -0400

    Run npm audit fix and fix graphql console warning

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, StaticQuery } from 'gatsby'
+import { Link, StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import VisuallyHidden from '@reach/visually-hidden'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
