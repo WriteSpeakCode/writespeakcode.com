be291eac616f624074993803141b79cc809f8335
Nicola Beuscher <nbeuscher@gmail.com>
2019-04-28 22:05:07 -0400

    Add to conference layout

--- src/layouts/conference/index.js
+++ src/layouts/conference/index.js
@@ -1,27 +1,88 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link, StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import VisuallyHidden from '@reach/visually-hidden'
 
 import '../base.css'
 import styles from './conference.module.css'
 
 const Layout = ({ children }) => (
-  <div className={styles.container}>
-    <section>
-      <h1>2019 Conference</h1>
-    </section>
-    <nav>
-      <ul>
-        <li>Logo</li>
-        <li>Speakers</li>
-        <li>Workshops</li>
-        <li>Location</li>
-        <li>Schedule</li>
-        <li>Sponsors</li>
-        <li>Tickets</li>
-      </ul>
-    </nav>
-    <main>{children}</main>
-  </div>
+  <StaticQuery
+    query={graphql`
+      query ConfLogoQuery {
+        file(
+          relativePath: { eq: "logos/wsc-horizontallogo-onecolor-blue.png" }
+        ) {
+          childImageSharp {
+            fixed(width: 250) {
+              ...GatsbyImageSharpFixed_tracedSVG
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <section className={styles.hero}>
+          <div className={styles.info}>
+            <Link to="/">
+              <VisuallyHidden>Write Speak Code Home</VisuallyHidden>
+              <Img
+                fixed={data.file.childImageSharp.fixed}
+                alt="Write/Speak/Code logo"
+                className={styles.logo}
+              />
+            </Link>
+            <h2 className={styles.subtitle}>
+              August 16-18, 2019 / San Francisco, CA
+            </h2>
+          </div>
+          <nav className={styles.nav}>
+            <ul className={styles.links}>
+              <li>
+                <a className="link" href="#about">
+                  About
+                </a>
+              </li>
+              <li>
+                <a className="link" href="#location">
+                  Location
+                </a>
+              </li>
+              <li>
+                <a className="link" href="#sponsors">
+                  Sponsors
+                </a>
+              </li>
+              <li>
+                <a
+                  className="link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://goo.gl/forms/gPMUnIwnvYMuh3Tp1"
+                >
+                  Call for Proposals
+                </a>
+              </li>
+              <li>
+                <a
+                  className="link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://writespeakcode-2019conference.eventbrite.com/"
+                >
+                  Get Tickets
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </section>
+
+        <main className={styles.bg}>{children}</main>
+      </>
+    )}
+  />
 )
 
 Layout.propTypes = {
--- src/pages/2019/index.js
+++ src/pages/2019/index.js
@@ -1,15 +1,26 @@
 import React from 'react'
 
-import Layout from '../../layouts/conference'
+import ConferenceLayout from '../../layouts/conference'
 import SEO from '../../components/seo'
 
+import Section from '../../components/section'
+
 const IndexPage = () => (
-  <Layout>
+  <ConferenceLayout>
     <SEO
       title="2019 Conference"
       keywords={[`tech`, `diversity`, `conference`]}
     />
-  </Layout>
+    <Section id="about">
+      <h1>About</h1>
+    </Section>
+    <Section id="location">
+      <h1>Location</h1>
+    </Section>
+    <Section id="sponsors">
+      <h1>Sponsors</h1>
+    </Section>
+  </ConferenceLayout>
 )
 
 export default IndexPage
