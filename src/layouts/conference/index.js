083a909e9451218f669966a5181d67c402b0700f
Nicola Beuscher <nbeuscher@gmail.com>
2019-06-05 20:44:43 -0400

    Update conf page

--- src/layouts/conference/index.js
+++ src/layouts/conference/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, StaticQuery, graphql } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
-import BackgroundImage from 'gatsby-background-image'
 import VisuallyHidden from '@reach/visually-hidden'
 
 import Footer from '../../components/footer'
@@ -10,9 +8,9 @@ import Footer from '../../components/footer'
 import '../base.css'
 import styles from './conference.module.css'
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
+const Layout = ({ children }) => {
+  const data = useStaticQuery(
+    graphql`
       query ConfLayoutQuery {
         logo: file(
           relativePath: { eq: "logos/wsc-horizontallogo-onecolor-blue.png" }
@@ -23,92 +21,24 @@ const Layout = ({ children }) => (
             }
           }
         }
-        background: file(relativePath: { eq: "photos/WSC-309.jpg" }) {
-          childImageSharp {
-            fluid(quality: 90, maxWidth: 4160) {
-              ...GatsbyImageSharpFluid_withWebp
-            }
-          }
-        }
       }
-    `}
-    render={data => {
-      const bgData = data.background.childImageSharp.fluid
-      const logoData = data.logo.childImageSharp.fixed
-      return (
-        <>
-          <BackgroundImage
-            Tag="section"
-            fluid={bgData}
-            backgroundColor={`var(--mint)`}
-            className={styles.heroBg}
-          >
-            <div className={styles.mintBg}>
-              <div className={styles.info}>
-                <Link to="/">
-                  <VisuallyHidden>Write Speak Code Home</VisuallyHidden>
-                  <Img
-                    fixed={logoData}
-                    alt="Write/Speak/Code logo"
-                    className={styles.logo}
-                  />
-                </Link>
-                <h2 className={styles.subtitle}>
-                  August 16-18, 2019 / San Francisco, CA
-                </h2>
-              </div>
-              <nav className={styles.nav}>
-                <ul className={styles.links}>
-                  <li>
-                    <a className="link" href="#about">
-                      About
-                    </a>
-                  </li>
-                  <li>
-                    <a className="link" href="#location">
-                      Location
-                    </a>
-                  </li>
-                  <li>
-                    <a className="link" href="#sponsors">
-                      Sponsors
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="link"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href="https://goo.gl/forms/gPMUnIwnvYMuh3Tp1"
-                    >
-                      Call for Proposals
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="link"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href="https://writespeakcode-2019conference.eventbrite.com/"
-                    >
-                      Get Tickets
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </BackgroundImage>
+    `
+  )
 
-          <main className={styles.main}>{children}</main>
-          <Footer />
-        </>
-      )
-    }}
-  />
-)
+  const logo = data.logo.childImageSharp.fixed
+  return (
+    <>
+      <header className={styles.header}>
+        <Link to="/">
+          <VisuallyHidden>Write Speak Code Home</VisuallyHidden>
+          <Img fixed={logo} alt="Write/Speak/Code logo" />
+        </Link>
+      </header>
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+      <main className={styles.main}>{children}</main>
+      <Footer />
+    </>
+  )
 }
 
 export default Layout
