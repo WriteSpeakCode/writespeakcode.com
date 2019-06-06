31b5374a894f7b864281b1c25121ba61d0e798fa
Nicola Beuscher <nbeuscher@gmail.com>
2019-06-05 21:50:13 -0400

    Create responsive menu component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,6 +3,8 @@ import { Link, useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import VisuallyHidden from '@reach/visually-hidden'
 
+import Menu from '../menu'
+
 import styles from './header.module.css'
 
 const Header = () => {
@@ -22,45 +24,17 @@ const Header = () => {
 
   return (
     <header className={styles.header}>
-      <nav>
-        <ul className={styles.list}>
-          <li>
-            <Link to="/">
-              <VisuallyHidden>Home</VisuallyHidden>
-              <Img
-                fixed={data.file.childImageSharp.fixed}
-                alt="Write/Speak/Code logo in full color"
-                className={styles.logo}
-              />
-            </Link>
-          </li>
-          <li>
-            <a className={styles.navLink} href="#community">
-              Community
-            </a>
-          </li>
-          <li>
-            <a className={styles.navLink} href="#conference">
-              Conference
-            </a>
-          </li>
-          <li>
-            <a className={styles.navLink} href="#own-your-expertise">
-              Own Your Expertise
-            </a>
-          </li>
-          <li>
-            <a className={styles.navLink} href="#chapter">
-              Chapters
-            </a>
-          </li>
-          <li>
-            <a className={styles.navLink} href="#sponsorship">
-              Sponsorship
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <div className={styles.container}>
+        <Link to="/">
+          <VisuallyHidden>Home</VisuallyHidden>
+          <Img
+            fixed={data.file.childImageSharp.fixed}
+            alt="Write/Speak/Code logo in full color"
+            className={styles.logo}
+          />
+        </Link>
+        <Menu />
+      </div>
     </header>
   )
 }
--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,20 +1,35 @@
 import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 import styles from './menu.module.css'
 
 const Menu = () => {
   const [menuIsOpen, setMenuIsOpen] = useState(false)
-  const menuText = menuIsOpen ? 'close menu' : 'open menu'
+  const icon = menuIsOpen ? faTimes : faBars
+
+  const menu = (
+    <nav className={styles.links}>
+      <a href="#community">Community</a>
+      <a href="#conference">Conference</a>
+      <a href="#own-your-expertise">Own Your Expertise</a>
+      <a href="#chapters">Chapters</a>
+      <a href="#sponsorship">Sponsorship</a>
+    </nav>
+  )
+
   return (
-    <>
-      <button
-        className={styles.button}
-        onClick={() => setMenuIsOpen(!menuIsOpen)}
-      >
-        {menuText}
-      </button>
-      {menuIsOpen && <div className={styles.menu}>Menu</div>}
-    </>
+    <div>
+      <div className={styles.large}>{menu}</div>
+      <div className={styles.small}>
+        <FontAwesomeIcon
+          icon={icon}
+          className={styles.menuButton}
+          onClick={() => setMenuIsOpen(!menuIsOpen)}
+        />
+        {menuIsOpen && menu}
+      </div>
+    </div>
   )
 }
 
