3fefca3bfbf62bfb51c5a11ed1229f1697bee0ca
Nicola B <deweydell@users.noreply.github.com>
2019-06-28 10:20:12 -0400

    Update index.js

--- src/components/notification/index.js
+++ src/components/notification/index.js
@@ -8,9 +8,9 @@ import styles from './notification.module.css'
 const Notification = () => (
   <div className={styles.notification}>
     <FontAwesomeIcon icon={faBullhorn} aria-hidden className={styles.icon} />
-    Register now for #WSC2019Conf &mdash; August 16-18 in San Francisco:{' '}
+    #WSC2019Conf &mdash; August 16&ndash;18 in San Francisco:{' '}
     <Link to="/2019/" className="link">
-      2019 Conference Page
+      Register now
     </Link>
   </div>
 )
