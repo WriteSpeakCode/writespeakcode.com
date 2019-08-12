7a91d963c26973f20b775fa7b00af5b45a70928f
Nicola Beuscher <nbeuscher@gmail.com>
2019-08-11 20:00:41 -0400

    Add speaker headshot to dialog button on schedule

--- src/components/speaker/index.js
+++ src/components/speaker/index.js
@@ -120,6 +120,13 @@ export const SpeakerButton = ({ speaker }) => {
   const [dialogIsOpen, setDialogIsOpen] = useState(false)
   return (
     <span className={styles.speakerBtn}>
+      {speaker.headshot && (
+        <img
+          src={speaker.headshot}
+          alt={`${speaker.name} headshot thumbnail`}
+        />
+      )}
+
       <button
         onClick={() => setDialogIsOpen(true)}
         className={styles.dialogButton}
