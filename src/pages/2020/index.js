1c9b76296b0e3603ef43c3494e190e65e8d138bf
Nicola B <nbeuscher@gmail.com>
2019-11-10 21:31:59 -0500

    Add link to mailing list and 2020 copy

--- src/pages/2020/index.js
+++ src/pages/2020/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import styles from './index.module.css'
+// import styles from './index.module.css'
 
 const Conference2020 = () => {
-  return <h1>hello</h1>
+  // return <h1>hello</h1>
 }
 
 export default Conference2020
